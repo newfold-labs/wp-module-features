fe1f37d9454166e4f87f897235d7a45be530e7a9
Evan Mullins <evanrm@gmail.com>
2024-05-09 18:14:05 -0400

    move callbacks into class

--- includes/Features.php
+++ includes/Features.php
@@ -35,89 +35,101 @@ class Features {
 		if ( function_exists( 'add_action' ) ) {
 
 			// Find and add all features to registry
-			add_action(
-				'plugins_loaded',
-				function () {
-					// Find extended instances of the Feature class and add to the Registry
-					foreach ( get_declared_classes() as $class ) {
-						if ( is_subclass_of( $class, 'NewfoldLabs\WP\Module\Features\Feature' ) ) {
-							// error_log( 'NewfoldLabs\WP\Module\Features child class found: '.$class );
-							// add class to registry and instantiate
-							self::$registry->set( $class );
-						}
-					}
-				},
-				1
-			);
+			add_action( 'plugins_loaded', array( __CLASS__, 'findFeatures' ), 1 );
 
 			// Register API endpoints
-			add_action(
-				'rest_api_init',
-				function () {
-					$api_instance = new FeaturesAPI();
-				}
-			);
+			add_action( 'rest_api_init', array( __CLASS__, 'registerRoutes' ));
 
 			// Add CLI commands
-			add_action(
-				'cli_init',
-				function () {
-					\WP_CLI::add_command(
-						'newfold features',
-						'NewfoldLabs\WP\Module\Features\FeaturesCLI',
-						array(
-							'shortdesc' => 'Operations for Newfold Features.',
-							'longdesc'  => 'Internal commands to handle features.' .
-											PHP_EOL . 'Subcommands: all, enable, disable, isEnabled.',
-						)
-					);
-				}
-			);
+			add_action( 'cli_init', array( __CLASS__, 'registerCLI' ));
 			
 			// Register API script and localized values
-			add_action(
-				'admin_enqueue_scripts',
-				function () {
-					// Register features API script
-					$scriptPath = container()->plugin()->url . 'vendor/newfold-labs/wp-module-features/static/js/features.js';
-					wp_register_script(
-						'newfold-features',
-						$scriptPath,
-						array( 'wp-api-fetch', 'newfold-features-local' ),
-						container()->plugin()->version,
-						true
-					);
-					// Register Localized Script with initial feature values
-					wp_register_script( 'newfold-features-local', null, null, container()->plugin()->version, true );
-					wp_localize_script(
-						'newfold-features-local',
-						'NewfoldFeatures',
-						array(
-							'features' => getFeatures(),
-							'restUrl'  => esc_url_raw( rest_url() ) . 'newfold-features/v1',
-						)
-					);
-				}
-			);
+			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'assets' ));
+
 		}
 
 		// Add default filter to make any feature null value return false
 		if ( function_exists( 'add_filter' ) ) {
 			add_filter(
 				'newfold/features/filter/isEnabled',
-				function ( $value ) {
-					// if feature state is null, return false
-					if ( ! isset( $value ) ) {
-						$value = false;
-					}
-					return $value;
-				},
+				array( __CLASS__, 'defaultIsEnabledFilter' ),
 				99, // low priority so modules can override easily if needed
 				1
 			);
 		}
 	}
 
+	/**
+	 * Find and add all feature classes to the registry
+	 */
+	public static function findFeatures () {
+		// Find extended instances of the Feature class and add to the Registry
+		foreach ( get_declared_classes() as $class ) {
+			if ( is_subclass_of( $class, 'NewfoldLabs\WP\Module\Features\Feature' ) ) {
+				// error_log( 'NewfoldLabs\WP\Module\Features child class found: '.$class );
+				// add class to registry and instantiate
+				self::$registry->set( $class );
+			}
+		}
+	}
+
+	/**
+	 * Register routes
+	 */
+	public static function registerRoutes () {
+		$api_instance = new FeaturesAPI();
+	}
+
+	/**
+	 * Add CLI commands
+	 */
+	public static function registerCLI () {
+		\WP_CLI::add_command(
+			'newfold features',
+			'NewfoldLabs\WP\Module\Features\FeaturesCLI',
+			array(
+				'shortdesc' => 'Operations for Newfold Features.',
+				'longdesc'  => 'Internal commands to handle features.' .
+								PHP_EOL . 'Subcommands: all, enable, disable, isEnabled.',
+			)
+		);
+	}
+
+	/**
+	 * Register API script and localized values
+	 */
+	public static function assets () {
+		// Register features API script
+		$scriptPath = container()->plugin()->url . 'vendor/newfold-labs/wp-module-features/static/js/features.js';
+		wp_register_script(
+			'newfold-features',
+			$scriptPath,
+			array( 'wp-api-fetch', 'newfold-features-local' ),
+			container()->plugin()->version,
+			true
+		);
+		// Register Localized Script with initial feature values
+		wp_register_script( 'newfold-features-local', null, null, container()->plugin()->version, true );
+		wp_localize_script(
+			'newfold-features-local',
+			'NewfoldFeatures',
+			array(
+				'features' => getFeatures(),
+				'restUrl'  => esc_url_raw( rest_url() ) . 'newfold-features/v1',
+			)
+		);
+	}
+	/**
+	 * Add default filter to make any feature null value return false
+	 */
+	public static function defaultIsEnabledFilter ( $value ) {
+		// if feature state is null, return false
+		if ( ! isset( $value ) ) {
+			$value = false;
+		}
+		return $value;
+	}
+	
 	/**
 	 * Get instance for singleton Features
 	 *
